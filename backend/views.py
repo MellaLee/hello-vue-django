22fb8f6736ff1c9285f3fca5ab78ea2231fffb7c
Lynn13 <lynnddd@126.com>
2018-12-02 10:38:43 +0800

    6, Label data done:complete all the siuations of auto mark

--- backend/views.py
+++ backend/views.py
@@ -99,10 +99,11 @@ def changeRequestIntoInt(request):
 
 @csrf_exempt
 def fetchLabelList(request):
+    # autoMark()
+    # return
     data = {}
     series = UrlLog.objects.filter(mark=1)
-    # data['total'] = series.count() 
-    data['total'] = 30894 
+    data['total'] = series.count() 
     intParams = changeRequestIntoInt(request)
     start = ( intParams['page'] - 1 ) * intParams['size']
     end = intParams['page'] * intParams['size']
@@ -113,15 +114,26 @@ def fetchLabelList(request):
 # 自动标记函数，默认访问次数总和<10的访问为善意访问，mark置为0
 def autoMark():
     allModel = UrlLog.objects.filter(mark=1)
-    for i in range(1, 100):
+    whilelist = ['qq.com', 'weixin.com', 'baidu.com', 'duba.net', 'sogou.com', 'ludashi', 'msg.71.am', 'cmcm.com', 'alibaba.com', '360.cn', 'sina', 'youdao.com', 'rising.com.cn', 'alicdn.com', 'aliyun.com', 'huajiao.com', 'ijinshan.com', '163.com', 'api.foxitreader.cn', 'api.mi.wifi.com', 'wx.qlogo.cn']
+    for i in range(1, 31):
         model = allModel[((i - 1) * 10000) : (10000 * i)]
+        # p = re.compile('^((25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(25[0-5]|2[0-4]\d|[01]?\d\d?)$')
         for item in model:
-            times = ast.literal_eval(item.times) 
-            total = 0
-            for i in times:
-                if (i > 0):
-                    total+=1
-            if (total < 10):
+            # for wl in whilelist:
+            #     if (wl in item.url):
+            #         item.mark = 0
+            #         item.save()
+            #         print (item.id)
+            #         continue
+            # else:
+            #     times = ast.literal_eval(item.times) 
+            #     total = 0
+            #     for i in times:
+            #         if (i > 0):
+            #             total+=1
+            #     if (total < 20):
+            # if p.match(item.url):
+            if (item.url != 'stat.funshion.net'):
                 item.mark = 0
                 item.save()
                 print (item.id)
